dd0830c093b2af14a4b415ec4ed656254eb263df
ahahahahah <kozyrnovaleksandr@gmail.com>
2024-04-26 08:38:10 +0300

    fixed cleans

--- clean.sh
+++ clean.sh
@@ -2,5 +2,5 @@
 
 
 rm ./*/*/*.aux ./*/*/*.bbl ./*/*/*.dvi ./*/*/*.fdb_* ./*/*/*.fls ./*/*/*.log ./*/*/*.out ./*/*/*.synctex ./*/*/*.gz ./*/*/*.toc ./*/*/*.ps 2> /dev/null
-rm *.aux *.bbl *.dvi *.fdb_* *.fls *.log *.out *.synctex *.gz *.toc *.ps 2> /dev/null
+rm *.aux *.bbl *.dvi *.fdb_* *.fls *.log *.out *.synctex *.gz *.toc *.ps current.pdf current.tex preamble.tex 2> /dev/null
 
--- clean_all.sh
+++ clean_all.sh
@@ -2,5 +2,5 @@
 
 
 rm ./*/*/*.aux ./*/*/*.bbl ./*/*/*.dvi ./*/*/*.fdb_* ./*/*/*.fls ./*/*/*.log ./*/*/*.out ./*/*/*.synctex ./*/*/*.gz ./*/*/*.toc ./*/*/*.ps 2> /dev/null
-rm *.aux *.bbl *.dvi *.fdb_* *.fls *.log *.out *.synctex *.gz *.toc *.ps current.tex preamble.tex 2> /dev/null
+rm *.aux *.bbl *.dvi *.fdb_* *.fls *.log *.out *.synctex *.gz *.toc *.ps current.tex preamble.tex current.pdf 2> /dev/null
 
